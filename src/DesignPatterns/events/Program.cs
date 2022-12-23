512fda0c014100dbbf3f9483bbf1843164a9726f
chrisgonper <chrizgabz9@gmail.com>
2022-12-23 16:07:14 -0600

    adding comments to event project

--- src/DesignPatterns/events/Program.cs
+++ src/DesignPatterns/events/Program.cs
@@ -8,11 +8,12 @@ namespace events
         {
             var priceReader = new PriceReader();
             var emailNotifier = new EmailNotifier(30000);
+            //attach de event
             priceReader.PriceRead += emailNotifier.Notify;
 
             priceReader.ReadCurrentPrice();
-
-            priceReader.PriceRead -= PriceReader_PriceRead;
+            //is a good practice to detach the event
+            priceReader.PriceRead -= emailNotifier.Notify;
 
             Console.ReadKey();
         }
