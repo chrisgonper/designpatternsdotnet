245f1af85a6aedc31083714097656ac8b0d58050
chrisgonper <chrizgabz9@gmail.com>
2023-08-31 15:46:31 -0600

    adding new property

--- src/DesignPatterns/Decorator/Person.cs
+++ src/DesignPatterns/Decorator/Person.cs
@@ -7,7 +7,7 @@ namespace Decorator
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime DOB { get; set; }
-
+        public string Address { get; set; }
         public Person(int id, string name, DateTime dOB)
         {
             Id = id;
