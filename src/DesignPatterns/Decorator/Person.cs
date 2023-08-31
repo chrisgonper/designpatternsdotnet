0bc808b2d9b72cc5f595e3fccbd5ab41cb903277
chrisgonper <chrizgabz9@gmail.com>
2023-08-31 16:00:20 -0600

    adding address to method constructor

--- src/DesignPatterns/Decorator/Person.cs
+++ src/DesignPatterns/Decorator/Person.cs
@@ -8,11 +8,12 @@ namespace Decorator
         public string Name { get; set; }
         public DateTime DOB { get; set; }
         public string Address { get; set; }
-        public Person(int id, string name, DateTime dOB)
+        public Person(int id, string name, DateTime dOB, string address)
         {
             Id = id;
             Name = name;
             DOB = dOB;
+            Address = address;
         }
         public override string ToString()
         {
